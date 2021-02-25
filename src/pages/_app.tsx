a2bfd8b4aa6edc18740292b666b3ebafbab80283
Jefferson William Machado <jeffersonwilliammachado@gmail.com>
2021-02-24 23:34:15 -0300

    feat: added meta description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,6 +23,7 @@ const MyApp: FC<AppProps> = ({ Component, pageProps, router }) => {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Formulário do Candidato | Intera" />
         <title>Formulário do Candidato | Intera</title>
       </Head>
       <ThemeProvider theme={theme}>
