41f8f816cb19ee8c740577471e94628d3bd3664b
Jefferson William Machado <jeffersonwilliammachado@gmail.com>
2021-03-08 18:00:39 -0300

    chore: env config when run test

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,5 +2,11 @@
 import 'jest-styled-components' // https://github.com/styled-components/jest-styled-components#snapshot-testing
 import '@testing-library/jest-dom/extend-expect'
 import 'mutationobserver-shim'
+import dotenv from 'dotenv'
+import path from 'path'
+
+const dotenvPath = path.join(__dirname, '..', '.env.local')
+
+dotenv.config({ path: dotenvPath })
 
 global.MutationObserver = global.window.MutationObserver
