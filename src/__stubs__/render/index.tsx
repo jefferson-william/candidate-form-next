13d732f537fd34b6599c850a5edfc8f9a2b097ca
Jefferson William Machado <jeffersonwilliammachado@gmail.com>
2021-03-17 20:15:03 -0300

    chore(tests): enhanced default render method

--- src/__stubs__/render/index.tsx
+++ src/__stubs__/render/index.tsx
@@ -1,30 +1,35 @@
-import React from 'react'
 import { Provider } from 'react-redux'
 import { RouterContext } from 'next-server/dist/lib/router-context'
 import { Router } from 'next/router'
 import { ThemeProvider } from 'styled-components'
 import { Theme } from '@material-ui/core'
-import { render } from '@testing-library/react'
+import { render, waitFor } from '@testing-library/react'
 import { store } from '~/store'
 import theme from '~/styles/theme'
 
 interface MyRenderOptions {
   router?: Partial<Router>
   theme?: Theme
+  store?: any
 }
 
-export const myRender = (Component: any, options?: MyRenderOptions) => {
+export const myRender = async (Component: any, options?: MyRenderOptions) => {
   const allOptions = {
     ...options,
   }
 
   allOptions.theme = options?.theme || theme
+  allOptions.store = options?.store || store()
 
-  return render(
+  const rendered = render(
     <RouterContext.Provider value={allOptions.router as any}>
-      <Provider store={store()}>
+      <Provider store={allOptions.store}>
         <ThemeProvider theme={allOptions.theme}>{Component}</ThemeProvider>
       </Provider>
     </RouterContext.Provider>
   )
+
+  await waitFor(() => rendered.container.querySelector('body'))
+
+  return rendered
 }
