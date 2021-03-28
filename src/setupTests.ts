715608729b14707ffa35006c9f21a8c68996cae2
Jefferson William Machado <jeffersonwilliammachado@gmail.com>
2021-03-27 21:42:59 -0300

    chore(test): ignored controlled componented console error in test environment

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -10,3 +10,22 @@ const dotenvPath = path.join(__dirname, '..', '.env.local')
 dotenv.config({ path: dotenvPath })
 
 global.MutationObserver = global.window.MutationObserver
+
+// eslint-disable-next-line no-console
+const originalError = console.error
+
+// eslint-disable-next-line no-console
+console.error = (...args) => {
+  const controlledComponentsWarning =
+    'Warning: A component is changing an uncontrolled input to be controlled.' +
+    '.*' +
+    'https://reactjs.org/link/controlled-components'
+
+  const regex = new RegExp(controlledComponentsWarning)
+
+  if (regex.test(args[0])) {
+    return
+  }
+
+  originalError.call(console, ...args)
+}
